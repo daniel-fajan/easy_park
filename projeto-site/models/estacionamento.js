bcc06103b607e31037fc24c5926380b987b11ec6
daniel-fajan <daniel.anselmi@bandtec.com.br>
2019-11-12 15:58:07 -0200

    padronizando repositorio

---
+++ projeto-site/models/estacionamento.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+
+    let Estacionamento = sequelize.define('Estacionamento',
+    {
+        id_estac:
+        {
+			field: 'id_estac',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+        },
+        
+        nome_estac:
+        {
+			field: 'nome_estac',
+			type: DataTypes.STRING,
+			allowNull: false
+        },
+        
+        cep:
+        {
+			field: 'cep',
+			type: DataTypes.CHAR,
+			allowNull: false
+        },
+        
+        numero:
+        {
+			field: 'numero',
+			type: DataTypes.INTEGER,
+			allowNull: false
+        },
+        
+        responsavel:
+        {
+            field: 'responsavel',
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    
+	{
+		tableName: 'tb_estacionamento', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Estacionamento;
+};
